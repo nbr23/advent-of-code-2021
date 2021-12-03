375d769eb5da595075812b07d7cfcb71db851ec6
Maxence <max@23.tf>
2021-12-03 03:18:30 +0000

    Add test input support

--- utils/utils/utils.go
+++ utils/utils/utils.go
@@ -2,13 +2,24 @@ package utils
 
 import (
 	"adventofcodego/utils/inputs"
+	"flag"
 	"fmt"
 )
 
 type Resolver func(string) interface{}
 
 func Solve(part1 Resolver, part2 Resolver, day int64) {
-	input := inputs.GetInput(day)
+	var input string
+
+	test_input := flag.Bool("test", false, "If set, uses ./dayX/testinput as input instead of the user specific input")
+	flag.Parse()
+
+	if *test_input {
+		input = string(inputs.ReadFile(fmt.Sprintf("./day%02d/testinput", day)))
+	} else {
+		input = inputs.GetInput(day)
+	}
+
 	res1 := part1(input)
 	fmt.Printf("Result 1: %v\n", res1)
 
