d00b1ba99974c85fe5da95197dc9b229a99cd30d
Maxence <max@23.tf>
2021-12-07 13:02:38 +0000

    DAy07 part 2

--- day07/day07.go
+++ day07/day07.go
@@ -24,11 +24,20 @@ func getMedian(positions []int) int {
 	return positions[middle]
 }
 
+func getMean(positions []int) int {
+	sum := 0
+	for _, v := range positions {
+		sum += v
+	}
+
+	return (sum + 1) / len(positions)
+}
+
 func part1(input string) interface{} {
 	result := 0
 	positions := inputToIntList(input)
-	sort.Ints(positions)
 	median := getMedian(positions)
+
 	for _, v := range positions {
 		if median > v {
 			result += median - v
@@ -39,8 +48,24 @@ func part1(input string) interface{} {
 	return result
 }
 
+func cumDiff(a int, b int) int {
+	diff := a - b
+	return diff * (diff + 1) / 2
+}
+
 func part2(input string) interface{} {
-	return nil
+	result := 0
+	positions := inputToIntList(input)
+	mean := getMean(positions)
+
+	for _, v := range positions {
+		if mean > v {
+			result += cumDiff(mean, v)
+		} else {
+			result += cumDiff(v, mean)
+		}
+	}
+	return result
 }
 
 func main() {
