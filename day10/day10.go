906e2cfc08bd1cf5d9043fda52833b986dbddaec
Maxence <max@23.tf>
2021-12-10 11:50:58 +0000

    Add day 10 part 2

--- day10/day10.go
+++ day10/day10.go
@@ -3,6 +3,7 @@ package main
 import (
 	// "adventofcodego/utils/inputs"
 	"adventofcodego/utils/utils"
+	"sort"
 	"strings"
 )
 
@@ -31,6 +32,13 @@ var scores = map[rune]int{
 	'>': 25137,
 }
 
+var scorescorrect = map[rune]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
+}
+
 func getScoreForLine(line string) int {
 	stack := make([]rune, 0)
 	for _, c := range line {
@@ -55,8 +63,33 @@ func part1(input string) interface{} {
 	return score
 }
 
+func getCorrectScoreForLine(line string) int {
+	stack := make([]rune, 0)
+	score := 0
+	for _, c := range line {
+		if opening[c] {
+			stack = append(stack, c)
+		} else {
+			if pairs[c] == stack[len(stack)-1] {
+				stack = stack[:len(stack)-1]
+			}
+		}
+	}
+	for i := len(stack) - 1; i >= 0; i-- {
+		score = score*5 + scorescorrect[pairs[stack[i]]]
+	}
+	return score
+}
+
 func part2(input string) interface{} {
-	return nil
+	scores := make([]int, 0)
+	for _, line := range strings.Split(input, "\n") {
+		if getScoreForLine(line) == 0 {
+			scores = append(scores, getCorrectScoreForLine(line))
+		}
+	}
+	sort.Slice(scores, func(i, j int) bool { return scores[i] > scores[j] })
+	return scores[len(scores)/2]
 }
 
 func main() {
