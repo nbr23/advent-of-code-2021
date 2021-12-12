6d10edd839ff56162563013ab55321f4cc8437ed
Maxence <max@23.tf>
2021-12-12 05:54:26 +0000

    Add day 12 part 1

---
+++ day12/day12.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	// "adventofcodego/utils/inputs"
+	"adventofcodego/utils/utils"
+	"strings"
+)
+
+func makeLinks(input string) map[string][]string {
+	links := make(map[string][]string)
+	for _, line := range strings.Split(input, "\n") {
+		sp := strings.Split(line, "-")
+		if _, ok := links[sp[0]]; ok {
+			links[sp[0]] = append(links[sp[0]], sp[1])
+		} else {
+			links[sp[0]] = make([]string, 1)
+			links[sp[0]][0] = sp[1]
+		}
+		if _, ok := links[sp[1]]; ok {
+			links[sp[1]] = append(links[sp[1]], sp[0])
+		} else {
+			links[sp[1]] = make([]string, 1)
+			links[sp[1]][0] = sp[0]
+		}
+	}
+	return links
+}
+
+func followPath(links map[string][]string, visited []string, current string) int {
+	score := 0
+	if current == "end" {
+		return 1
+	}
+	if current != "start" && strings.ToLower(current) == current {
+		for i := range visited {
+			if current == visited[i] {
+				return 0
+			}
+		}
+	}
+	visited = append(visited, current)
+	for _, next := range links[current] {
+		if next == "start" {
+			continue
+		}
+		newvisited := make([]string, len(visited))
+		copy(newvisited, visited)
+		score += followPath(links, newvisited, next)
+	}
+	return score
+}
+
+func part1(input string) interface{} {
+	links := makeLinks(input)
+	return followPath(links, make([]string, 0), "start")
+}
+
+func part2(input string) interface{} {
+	return nil
+}
+
+func main() {
+	var day int64 = 12
+
+	utils.Solve(part1, part2, day)
+}
