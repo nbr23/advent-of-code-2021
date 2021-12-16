a9416ddda72b41dda8e2da63024a9a78ed4362a8
Maxence <max@23.tf>
2021-12-16 02:31:53 +0000

    Add day 15 part 2

--- day15/day15.go
+++ day15/day15.go
@@ -5,7 +5,10 @@ import (
 
 	"adventofcodego/utils/utils"
 	"fmt"
+	"math"
 	"strings"
+
+	"github.com/pkg/profile"
 )
 
 var DAY int = 15
@@ -33,46 +36,124 @@ func parseMatrix(input string) []int {
 }
 
 func getMinCost(a, b int) int {
-	if a == -1 || b < a {
+	if b < a {
 		return b
 	}
 	return a
 }
 
-func computeCosts(matrix []int, size int) []int {
-	costs := make([]int, HEIGHT*WIDTH)
-	for x := 0; x < WIDTH*; x++ {
-		for y := 0; y < HEIGHT; y++ {
-			if x == 0 && y == 0 {
-				costs[x+y*WIDTH] = 0
-			} else if x == 0 {
-				costs[x+y*WIDTH] = matrix[x+y*WIDTH] + costs[x+(y-1)*WIDTH]
-			} else if y == 0 {
-				costs[x+y*WIDTH] = matrix[x+y*WIDTH] + costs[x-1+y*WIDTH]
-			} else {
-				costs[x+y*WIDTH] = matrix[x+y*WIDTH] + getMinCost(costs[x-1+y*WIDTH], costs[x+(y-1)*WIDTH])
+func getCostXY(matrix []int, x int, y int) int {
+	cost := (matrix[(x%WIDTH)+(y%WIDTH)*WIDTH] + x/WIDTH + y/HEIGHT)
+	return cost%10 + cost/10
+}
+
+func computeCosts(matrix []int, size int, costs []int, visited []bool, x int, y int) {
+	if visited[len(visited)-1] {
+		return
+	}
+	visited[x+y*WIDTH*size] = true
+	for nx := int(math.Max(0, float64(x-1))); nx < int(math.Min(float64(WIDTH*size), float64(x+2))); nx++ {
+		for ny := int(math.Max(0, float64(y-1))); ny < int(math.Min(float64(HEIGHT*size), float64(y+2))); ny++ {
+			if nx == x && ny == y || nx != x && ny != y || visited[nx+ny*WIDTH*size] {
+				continue
 			}
+			costs[nx+ny*WIDTH*size] = getMinCost(costs[nx+ny*WIDTH*size], getCostXY(matrix, nx, ny)+costs[x+y*WIDTH*size])
+			computeCosts(matrix, size, costs, visited, nx, ny)
+		}
+	}
+}
+
+func Dijkstra(matrix []int, size int) map[point]int {
+	active_nodes := make(map[point]int)
+	visited := make([]bool, HEIGHT*WIDTH*size*size)
+	current := point{0, 0}
+	costs := initCosts(size)
+
+	active_nodes[point{0, 0}] = 0
+
+	for {
+		for nx := int(math.Max(0, float64(current.x-1))); nx < int(math.Min(float64(WIDTH*size), float64(current.x+2))); nx++ {
+			for ny := int(math.Max(0, float64(current.y-1))); ny < int(math.Min(float64(HEIGHT*size), float64(current.y+2))); ny++ {
+				if nx == current.x && ny == current.y || nx != current.x && ny != current.y || visited[nx+ny*WIDTH*size] {
+					continue
+				}
+				costs[point{nx, ny}] = getMinCost(costs[point{nx, ny}], getCostXY(matrix, nx, ny)+costs[point{current.x, current.y}])
+				active_nodes[point{nx, ny}] = costs[point{nx, ny}]
+			}
+		}
+
+		// remove from active nodes
+		delete(active_nodes, current)
+
+		// mark as visited
+		visited[current.x+current.y*WIDTH*size] = true
+
+		// search for next node
+		current = getNextNode(active_nodes, costs)
+		if len(active_nodes) == 0 || visited[WIDTH*size*HEIGHT*size-1] {
+			break
 		}
 	}
 	return costs
 }
 
-func printMatrix(matrix []int) {
-	for y := 0; y < HEIGHT; y++ {
-		fmt.Println(matrix[y*HEIGHT : y*HEIGHT+WIDTH])
+func getNextNode(active_nodes map[point]int, costs map[point]int) point {
+	minv := math.MaxInt
+	var current point
+	for pt := range active_nodes {
+		val := costs[pt]
+		if val < minv {
+			minv = val
+			current = point{pt.x, pt.y}
+		}
+	}
+	return current
+}
+
+func printMatrix(matrix []int, size int) {
+	for y := 0; y < HEIGHT*size; y++ {
+		for x := 0; x < WIDTH*size; x++ {
+			fmt.Printf("%v ", getCostXY(matrix, x, y))
+		}
+		fmt.Println()
 	}
 }
 
+func printCosts(matrix map[point]int, size int) {
+	for y := 0; y < HEIGHT*size; y++ {
+		for x := 0; x < WIDTH*size; x++ {
+			fmt.Printf("%d ", matrix[point{x, y}])
+		}
+		fmt.Println()
+	}
+}
+
+func initCosts(size int) map[point]int {
+	costs := make(map[point]int)
+	for y := 0; y < HEIGHT*size; y++ {
+		for x := 0; x < WIDTH*size; x++ {
+			costs[point{x, y}] = math.MaxInt
+		}
+	}
+	costs[point{0, 0}] = 0
+	return costs
+}
+
 func part1(input string) interface{} {
+	size := 1
 	matrix := parseMatrix(input)
-	costs := computeCosts(matrix)
-	return costs[len(costs)-1]
+	costs := Dijkstra(matrix, size)
+	return costs[point{WIDTH*size - 1, HEIGHT*size - 1}]
 }
 
 func part2(input string) interface{} {
-	return nil
+	size := 5
+	matrix := parseMatrix(input)
+	costs := Dijkstra(matrix, size)
+	return costs[point{WIDTH*size - 1, HEIGHT*size - 1}]
 }
 
 func main() {
+	defer profile.Start().Stop()
 	utils.Solve(part1, part2, DAY)
 }
