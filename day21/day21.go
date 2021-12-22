62545be0c5628bc828669a45ce9adebaf9cd0240
Maxence <max@23.tf>
2021-12-22 03:47:26 +0000

    Add day 21 part 2

--- day21/day21.go
+++ day21/day21.go
@@ -61,8 +61,79 @@ func part1(input string) interface{} {
 	}
 }
 
+var POSSIBILITIES [][]int
+
+func possibilities() [][]int {
+	p := make([][]int, 3*3*3)
+	c := 0
+	for i := 1; i <= 3; i++ {
+		for j := 1; j <= 3; j++ {
+			for k := 1; k <= 3; k++ {
+				p[c] = []int{i, j, k}
+				c++
+			}
+		}
+	}
+	return p
+}
+
+type state struct {
+	p1score int
+	p2score int
+	p1pos   int
+	p2pos   int
+}
+
+type wins struct {
+	p1wins int
+	p2wins int
+}
+
+var CACHE map[state]wins
+
+func playTurn(p1score int, p2score int, pos1 int, pos2 int) (int, int) {
+	if p1score >= 21 {
+		return 1, 0
+	}
+	p1count := 0
+	p2count := 0
+
+	for _, d1 := range POSSIBILITIES {
+		pos1b := pos1 + d1[0] + d1[1] + d1[2]
+		pos1b = pos1b % 10
+		if pos1b == 0 {
+			pos1b = 10
+		}
+		p1bscore := p1score + pos1b
+
+		if p1bscore >= 21 {
+			p1count++
+			continue
+		}
+
+		if s, ok := CACHE[state{p2score, p1bscore, pos2, pos1b}]; ok {
+			p1count += s.p2wins
+			p2count += s.p1wins
+		} else {
+			p2c, p1c := playTurn(p2score, p1bscore, pos2, pos1b)
+			CACHE[state{p2score, p1bscore, pos2, pos1b}] = wins{p2c, p1c}
+			p1count += p1c
+			p2count += p2c
+		}
+	}
+	return p1count, p2count
+}
+
 func part2(input string) interface{} {
-	return nil
+	CACHE = make(map[state]wins)
+	POSSIBILITIES = possibilities()
+	p1, p2 := parseStartingPositions(strings.Split(input, "\n"))
+	s1, s2 := playTurn(0, 0, p1, p2)
+
+	if s1 > s2 {
+		return s1
+	}
+	return s2
 }
 
 func main() {
